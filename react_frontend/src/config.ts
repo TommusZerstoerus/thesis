428512b96695a1cfd444d887dd4c6a26e7cb87d1
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2024-11-27 11:28:28 +0100

    Update react config to match flutter

--- react_frontend/src/config.ts
+++ react_frontend/src/config.ts
@@ -2,4 +2,4 @@ export const cubeCount = 100
 export const listSize = 10000
 export const dataSize = 10000
 export const fibonacciNumber = 45
-export const stateManagementSize = 10000
+export const stateManagementSize = 1000
