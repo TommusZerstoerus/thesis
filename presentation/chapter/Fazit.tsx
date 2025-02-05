7feae1dcdcba630de8dee83e010e82c3586bfbd1
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2025-02-05 19:28:30 +0100

    feat: fill resume slide with content

--- presentation/chapter/Fazit.tsx
+++ presentation/chapter/Fazit.tsx
@@ -1,4 +1,4 @@
-import {Text, FlexBox, Heading, ListItem, Slide, UnorderedList} from "spectacle";
+import {Appear, Box, FlexBox, Heading, Image, ListItem, Slide, Text, UnorderedList} from "spectacle";
 import React from "react";
 
 export const Fazit = () => {
@@ -9,9 +9,35 @@ export const Fazit = () => {
             </FlexBox>
             <UnorderedList>
                 <ListItem>
-                    <Text>Hackerman</Text>
+                    <Text>
+                        Viele Aspekte beeinflussen die Wahl (Design, Kenntnisse, usw.)
+                    </Text>
+                </ListItem>
+                <ListItem>
+                    <Text>
+                        Im Leistungsbereich überzeugt Flutter
+                    </Text>
+                </ListItem>
+                <ListItem>
+                    <Text>
+                        Stetige Weiterentwicklung oder Anpassung beeinflussen die Ergebnisse
+                    </Text>
                 </ListItem>
             </UnorderedList>
+            <Appear>
+                <Box mt={100}>
+                    <FlexBox justifyContent="center">
+                        <Text fontStyle="italic" fontSize={40}>Vielen Dank für eure Aufmerksamkeit! 👋</Text>
+                    </FlexBox>
+                    <FlexBox width="100%" justifyContent="space-between">
+                        <Image width="200"
+                               src="https://storage.googleapis.com/cms-storage-bucket/847ae81f5430402216fd.svg"
+                               alt="Flutter Logo"/>
+                        <Image width="200" src="https://cdn.worldvectorlogo.com/logos/react-native-1.svg"
+                               alt="React Native Logo"/>
+                    </FlexBox>
+                </Box>
+            </Appear>
         </Slide>
     )
 }
